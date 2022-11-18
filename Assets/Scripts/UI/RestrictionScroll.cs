95424448c84c629929b3932f5110166d44b2fcb8
Antis <Antis28@mail.ru>
2022-11-18 23:47:17 +0300

    refactoring

--- Assets/Scripts/UI/RestrictionScroll.cs
+++ Assets/Scripts/UI/RestrictionScroll.cs
@@ -12,33 +12,26 @@ public class RestrictionScroll : MonoBehaviour
     private PanelInfo startPanel;
     private PanelInfo playerPanel;
     private PanelInfo powerPanel;
-
-    struct PanelInfo
-    {
-        public float yPos;
-        public float top;
-        public float bottom;
-    }
-
+    
     private void Start()
     {
         startPanel = new PanelInfo()
         {
-            yPos = 0,
-            top = 0,
-            bottom = heightPanel * 0.10f
+            YPos = 0,
+            Top = 0,
+            Bottom = heightPanel * 0.10f
         };
         playerPanel = new PanelInfo()
         {
-            yPos = heightPanel,
-            top = heightPanel - heightPanel * 0.2f,
-            bottom = heightPanel + heightPanel * 0.2f
+            YPos = heightPanel,
+            Top = heightPanel - heightPanel * 0.2f,
+            Bottom = heightPanel + heightPanel * 0.2f
         };
         powerPanel = new PanelInfo()
         {
-            yPos = heightPanel * 2,
-            top = heightPanel * 2 - heightPanel * 0.2f,
-            bottom = heightPanel * 2 + heightPanel * 0.2f
+            YPos = heightPanel * 2,
+            Top = heightPanel * 2 - heightPanel * 0.2f,
+            Bottom = heightPanel * 2 + heightPanel * 0.2f
         };
     }
 
@@ -46,63 +39,30 @@ public class RestrictionScroll : MonoBehaviour
     {
         var yPos = _rectTransform.localPosition.y;
 
-        if (CheckStartPanel(yPos)) return;
+        if (CheckPanel(yPos, startPanel)) return;
 
-        if (CheckPlayerPanel(yPos)) return;
+        if (CheckPanel(yPos, playerPanel)) return;
 
-        CheckPowerPanel(yPos);
+        if (CheckPanel(yPos, powerPanel)) return;
     }
 
     private bool CheckPanel(float yPos, PanelInfo panelInfo )
     {
-        if (yPos > panelInfo.top && yPos < panelInfo.bottom)
+        if (yPos > panelInfo.Top && yPos < panelInfo.Bottom)
         {
             var pos = _rectTransform.localPosition;
-            pos.y = panelInfo.yPos;
+            pos.y = panelInfo.YPos;
             _rectTransform.localPosition = pos;
             return true;
         }
 
         return false;
     }
-
-    private bool CheckPowerPanel(float yPos)
+    
+    private struct PanelInfo
     {
-        return CheckPanel(yPos, powerPanel);
-        // if (yPos > powerPanelTop && yPos < powerPanelBottom)
-        // {
-        //     var pos = _rectTransform.localPosition;
-        //     pos.y = heightPanel * 2;
-        //     _rectTransform.localPosition = pos;
-        //     return;
-        // }
-    }
-
-    private bool CheckPlayerPanel(float yPos)
-    {
-        return CheckPanel(yPos, playerPanel);
-        // if (yPos > playerPanelTop && yPos < playerPanelBottom)
-        // {
-        //     var pos = _rectTransform.localPosition;
-        //     pos.y = heightPanel;
-        //     _rectTransform.localPosition = pos;
-        //     return true;
-        // }
-        //
-        // return false;
-    }
-
-    private bool CheckStartPanel(float yPos)
-    {
-        return CheckPanel(yPos, startPanel);
-        // if (yPos < startPanel)
-        // {
-        //     var pos = _rectTransform.localPosition;
-        //     pos.y = 0;
-        //     _rectTransform.localPosition = pos;
-        //     return true;
-        // }
-        //
-        // return false;
+        public float YPos;
+        public float Top;
+        public float Bottom;
     }
 }
